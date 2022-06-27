fb14859a81db8de8c11975408f0c27614ec82d64
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2022-06-27 19:40:07 +0530

    Update misc.py

--- Tianabot/modules/helper_funcs/misc.py
+++ Tianabot/modules/helper_funcs/misc.py
@@ -74,9 +74,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     else:
         pairs += [
                   [
-                    EqInlineKeyboardButton("╰✰ Cʜᴀɴɴᴇʟ", url="t.me/TianaxUpdates"), 
-                    EqInlineKeyboardButton("「 Bᴀᴄᴋ 」", callback_data="tiana_"), 
-                    EqInlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ ✰╮", url="t.me/TianaxSupport")
+                    EqInlineKeyboardButton("╰✰ Cʜᴀɴɴᴇʟ", url="t.me/Agora_Robots"), 
+                    EqInlineKeyboardButton("「 Bᴀᴄᴋ 」", callback_data="Agora_"), 
+                    EqInlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ ✰╮", url="t.me/Agora_Army")
                   ]
                  ]
 
